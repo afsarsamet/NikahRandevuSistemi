c6f8bf7aac113fd3d6f18f365e93ac694aa3818a
Samet Afşar <sametafs9@gmail.com>
2025-12-10 22:37:47 +0300

    Refactor FrmRandevuOlustur form layout and logic

--- FrmRandevuOlustur.Designer.cs
+++ FrmRandevuOlustur.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace NikahRandevu0
+namespace NikahRandevu0
 {
     partial class FrmRandevuOlustur
     {
@@ -59,13 +59,17 @@
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.btnRandevuOlustur = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCikis = new System.Windows.Forms.Button();
+            this.btnRandevularim = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(302, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(244, 27);
             this.label1.TabIndex = 0;
@@ -74,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 62);
+            this.label2.Location = new System.Drawing.Point(304, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 16);
             this.label2.TabIndex = 1;
@@ -83,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 99);
+            this.label3.Location = new System.Drawing.Point(306, 107);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 16);
             this.label3.TabIndex = 2;
@@ -92,7 +96,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 145);
+            this.label4.Location = new System.Drawing.Point(306, 153);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(88, 16);
             this.label4.TabIndex = 3;
@@ -100,15 +104,16 @@
             // 
             // txtDamatTC
             // 
-            this.txtDamatTC.Location = new System.Drawing.Point(110, 56);
+            this.txtDamatTC.Location = new System.Drawing.Point(400, 64);
             this.txtDamatTC.MaxLength = 11;
             this.txtDamatTC.Name = "txtDamatTC";
             this.txtDamatTC.Size = new System.Drawing.Size(146, 22);
             this.txtDamatTC.TabIndex = 4;
+            this.txtDamatTC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDamatTC_KeyPress);
             // 
             // txtDamatAdSoyad
             // 
-            this.txtDamatAdSoyad.Location = new System.Drawing.Point(110, 93);
+            this.txtDamatAdSoyad.Location = new System.Drawing.Point(400, 101);
             this.txtDamatAdSoyad.Name = "txtDamatAdSoyad";
             this.txtDamatAdSoyad.Size = new System.Drawing.Size(146, 22);
             this.txtDamatAdSoyad.TabIndex = 5;
@@ -117,7 +122,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(418, 9);
+            this.label5.Location = new System.Drawing.Point(708, 17);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(232, 27);
             this.label5.TabIndex = 7;
@@ -125,23 +130,24 @@
             // 
             // txtGelinAdSoyad
             // 
-            this.txtGelinAdSoyad.Location = new System.Drawing.Point(514, 96);
+            this.txtGelinAdSoyad.Location = new System.Drawing.Point(804, 104);
             this.txtGelinAdSoyad.Name = "txtGelinAdSoyad";
             this.txtGelinAdSoyad.Size = new System.Drawing.Size(146, 22);
             this.txtGelinAdSoyad.TabIndex = 13;
             // 
             // txtGelinTC
             // 
-            this.txtGelinTC.Location = new System.Drawing.Point(514, 59);
+            this.txtGelinTC.Location = new System.Drawing.Point(804, 67);
             this.txtGelinTC.MaxLength = 11;
             this.txtGelinTC.Name = "txtGelinTC";
             this.txtGelinTC.Size = new System.Drawing.Size(146, 22);
             this.txtGelinTC.TabIndex = 12;
+            this.txtGelinTC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGelinTC_KeyPress);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(420, 148);
+            this.label6.Location = new System.Drawing.Point(710, 156);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(88, 16);
             this.label6.TabIndex = 11;
@@ -150,7 +156,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(420, 102);
+            this.label7.Location = new System.Drawing.Point(710, 110);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(67, 16);
             this.label7.TabIndex = 10;
@@ -159,7 +165,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(418, 65);
+            this.label8.Location = new System.Drawing.Point(708, 73);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(90, 16);
             this.label8.TabIndex = 9;
@@ -167,14 +173,14 @@
             // 
             // dtGelinDogum
             // 
-            this.dtGelinDogum.Location = new System.Drawing.Point(514, 140);
+            this.dtGelinDogum.Location = new System.Drawing.Point(804, 148);
             this.dtGelinDogum.Name = "dtGelinDogum";
             this.dtGelinDogum.Size = new System.Drawing.Size(200, 22);
             this.dtGelinDogum.TabIndex = 14;
             // 
             // dtDamatDogum
             // 
-            this.dtDamatDogum.Location = new System.Drawing.Point(110, 139);
+            this.dtDamatDogum.Location = new System.Drawing.Point(400, 147);
             this.dtDamatDogum.Name = "dtDamatDogum";
             this.dtDamatDogum.Size = new System.Drawing.Size(200, 22);
             this.dtDamatDogum.TabIndex = 15;
@@ -183,7 +189,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label9.Location = new System.Drawing.Point(15, 226);
+            this.label9.Location = new System.Drawing.Point(303, 230);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(178, 24);
             this.label9.TabIndex = 16;
@@ -191,23 +197,24 @@
             // 
             // txtSahit1AdSoyad
             // 
-            this.txtSahit1AdSoyad.Location = new System.Drawing.Point(112, 337);
+            this.txtSahit1AdSoyad.Location = new System.Drawing.Point(402, 345);
             this.txtSahit1AdSoyad.Name = "txtSahit1AdSoyad";
             this.txtSahit1AdSoyad.Size = new System.Drawing.Size(146, 22);
             this.txtSahit1AdSoyad.TabIndex = 20;
             // 
             // txtSahit1TC
             // 
-            this.txtSahit1TC.Location = new System.Drawing.Point(112, 300);
+            this.txtSahit1TC.Location = new System.Drawing.Point(402, 308);
             this.txtSahit1TC.MaxLength = 11;
             this.txtSahit1TC.Name = "txtSahit1TC";
             this.txtSahit1TC.Size = new System.Drawing.Size(146, 22);
             this.txtSahit1TC.TabIndex = 19;
+            this.txtSahit1TC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSahit1TC_KeyPress);
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(16, 343);
+            this.label10.Location = new System.Drawing.Point(306, 351);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(67, 16);
             this.label10.TabIndex = 18;
@@ -216,7 +223,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(14, 306);
+            this.label11.Location = new System.Drawing.Point(304, 314);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(90, 16);
             this.label11.TabIndex = 17;
@@ -226,7 +233,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Gadugi", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(13, 263);
+            this.label12.Location = new System.Drawing.Point(303, 271);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(67, 20);
             this.label12.TabIndex = 21;
@@ -236,7 +243,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Gadugi", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(413, 263);
+            this.label13.Location = new System.Drawing.Point(703, 271);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(67, 20);
             this.label13.TabIndex = 22;
@@ -244,23 +251,24 @@
             // 
             // txtSahit2AdSoyad
             // 
-            this.txtSahit2AdSoyad.Location = new System.Drawing.Point(514, 334);
+            this.txtSahit2AdSoyad.Location = new System.Drawing.Point(804, 342);
             this.txtSahit2AdSoyad.Name = "txtSahit2AdSoyad";
             this.txtSahit2AdSoyad.Size = new System.Drawing.Size(146, 22);
             this.txtSahit2AdSoyad.TabIndex = 26;
             // 
             // txtSahit2TC
             // 
-            this.txtSahit2TC.Location = new System.Drawing.Point(514, 297);
+            this.txtSahit2TC.Location = new System.Drawing.Point(804, 305);
             this.txtSahit2TC.MaxLength = 11;
             this.txtSahit2TC.Name = "txtSahit2TC";
             this.txtSahit2TC.Size = new System.Drawing.Size(146, 22);
             this.txtSahit2TC.TabIndex = 25;
+            this.txtSahit2TC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSahit2TC_KeyPress);
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(413, 343);
+            this.label14.Location = new System.Drawing.Point(703, 351);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(67, 16);
             this.label14.TabIndex = 24;
@@ -269,7 +277,7 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(414, 303);
+            this.label15.Location = new System.Drawing.Point(704, 311);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(90, 16);
             this.label15.TabIndex = 23;
@@ -279,7 +287,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label16.Location = new System.Drawing.Point(15, 408);
+            this.label16.Location = new System.Drawing.Point(305, 416);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(216, 24);
             this.label16.TabIndex = 27;
@@ -288,14 +296,14 @@
             // cmbSaat
             // 
             this.cmbSaat.FormattingEnabled = true;
-            this.cmbSaat.Location = new System.Drawing.Point(110, 510);
+            this.cmbSaat.Location = new System.Drawing.Point(400, 518);
             this.cmbSaat.Name = "cmbSaat";
             this.cmbSaat.Size = new System.Drawing.Size(121, 24);
             this.cmbSaat.TabIndex = 28;
             // 
             // dtRandevuTarih
             // 
-            this.dtRandevuTarih.Location = new System.Drawing.Point(110, 463);
+            this.dtRandevuTarih.Location = new System.Drawing.Point(400, 471);
             this.dtRandevuTarih.Name = "dtRandevuTarih";
             this.dtRandevuTarih.Size = new System.Drawing.Size(200, 22);
             this.dtRandevuTarih.TabIndex = 29;
@@ -304,7 +312,7 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(16, 468);
+            this.label17.Location = new System.Drawing.Point(306, 476);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(38, 16);
             this.label17.TabIndex = 30;
@@ -313,7 +321,7 @@
             // label18
             // 
             this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(19, 518);
+            this.label18.Location = new System.Drawing.Point(309, 526);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(35, 16);
             this.label18.TabIndex = 31;
@@ -324,20 +332,52 @@
             this.btnRandevuOlustur.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnRandevuOlustur.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnRandevuOlustur.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnRandevuOlustur.Location = new System.Drawing.Point(423, 444);
+            this.btnRandevuOlustur.Location = new System.Drawing.Point(794, 455);
             this.btnRandevuOlustur.Name = "btnRandevuOlustur";
-            this.btnRandevuOlustur.Size = new System.Drawing.Size(343, 90);
+            this.btnRandevuOlustur.Size = new System.Drawing.Size(229, 87);
             this.btnRandevuOlustur.TabIndex = 32;
             this.btnRandevuOlustur.Text = "RANDEVUYU ONAYLA";
             this.btnRandevuOlustur.UseVisualStyleBackColor = false;
             this.btnRandevuOlustur.Click += new System.EventHandler(this.btnRandevuOlustur_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCikis);
+            this.panel1.Controls.Add(this.btnRandevularim);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 610);
+            this.panel1.TabIndex = 33;
+            // 
+            // buttonCikis
+            // 
+            this.buttonCikis.Location = new System.Drawing.Point(0, 533);
+            this.buttonCikis.Name = "buttonCikis";
+            this.buttonCikis.Size = new System.Drawing.Size(230, 77);
+            this.buttonCikis.TabIndex = 1;
+            this.buttonCikis.Text = "ÇIKIŞ";
+            this.buttonCikis.UseVisualStyleBackColor = true;
+            this.buttonCikis.Click += new System.EventHandler(this.buttonCikis_Click);
+            // 
+            // btnRandevularim
+            // 
+            this.btnRandevularim.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRandevularim.Location = new System.Drawing.Point(0, 0);
+            this.btnRandevularim.Name = "btnRandevularim";
+            this.btnRandevularim.Size = new System.Drawing.Size(230, 77);
+            this.btnRandevularim.TabIndex = 0;
+            this.btnRandevularim.Text = "RANDEVULARIM";
+            this.btnRandevularim.UseVisualStyleBackColor = true;
+            this.btnRandevularim.Click += new System.EventHandler(this.btnRandevularim_Click);
             // 
             // FrmRandevuOlustur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(815, 571);
+            this.ClientSize = new System.Drawing.Size(1133, 610);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnRandevuOlustur);
             this.Controls.Add(this.label18);
             this.Controls.Add(this.label17);
@@ -369,9 +409,12 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "FrmRandevuOlustur";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmRandevuOlustur";
             this.Load += new System.EventHandler(this.FrmRandevuOlustur_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -410,5 +453,8 @@
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label18;
         private System.Windows.Forms.Button btnRandevuOlustur;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRandevularim;
+        private System.Windows.Forms.Button buttonCikis;
     }
 }
