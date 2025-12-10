b9f079512be177061e09806ffc3821d16c080dcd
Samet Afşar <sametafs9@gmail.com>
2025-12-10 22:43:03 +0300

    TC Algoritması

---
+++ TCKontrolcs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Araclar
+{
+   
+    public static bool TCKontrol(string tcNo)
+    {
+        if (tcNo.Length != 11) return false;
+
+        int toplam = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            toplam += Convert.ToInt32(tcNo[i].ToString());
+        }
+
+        int sonHane = Convert.ToInt32(tcNo[10].ToString());
+
+        return (toplam % 10 == sonHane);
+    }
+}
