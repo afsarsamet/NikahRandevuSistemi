401b82fe9222185e3ea7b7571f1ef31101078166
Berkant Kılıç <kilicberkant2021@gmail.com>
2025-12-10 22:48:05 +0300

    Add files via upload

---
+++ Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikahRandevu0
+{
+    internal class Session
+    {
+        public static int UserID { get; set; }
+        public static string AdSoyad { get; set; }
+        public static bool IsAdmin { get; set; }
+        public static string Email { get; set; }
+    }
+}
