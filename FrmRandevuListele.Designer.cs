e28ff2d94b98df0415cedebdfa52f553ca88da81
Samet Afşar <sametafs9@gmail.com>
2025-12-10 22:41:25 +0300

    Enhance FrmRandevuListele with return button
    Added a new button for returning to the previous screen and updated the appearance of the DataGridView.

--- FrmRandevuListele.Designer.cs
+++ FrmRandevuListele.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace NikahRandevu0
+namespace NikahRandevu0
 {
     partial class FrmRandevuListele
     {
@@ -30,13 +30,16 @@
         {
             this.dgvRandevular = new System.Windows.Forms.DataGridView();
             this.btnIptalEt = new System.Windows.Forms.Button();
+            this.buttonGeriDon = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRandevular)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvRandevular
             // 
+            this.dgvRandevular.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dgvRandevular.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgvRandevular.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRandevular.Location = new System.Drawing.Point(12, 25);
+            this.dgvRandevular.Location = new System.Drawing.Point(12, 12);
             this.dgvRandevular.MultiSelect = false;
             this.dgvRandevular.Name = "dgvRandevular";
             this.dgvRandevular.RowHeadersWidth = 51;
@@ -48,22 +51,35 @@
             // btnIptalEt
             // 
             this.btnIptalEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnIptalEt.Location = new System.Drawing.Point(12, 388);
+            this.btnIptalEt.Location = new System.Drawing.Point(279, 375);
             this.btnIptalEt.Name = "btnIptalEt";
-            this.btnIptalEt.Size = new System.Drawing.Size(983, 125);
+            this.btnIptalEt.Size = new System.Drawing.Size(437, 123);
             this.btnIptalEt.TabIndex = 1;
             this.btnIptalEt.Text = "RANDEVUYU İPTAL ET";
             this.btnIptalEt.UseVisualStyleBackColor = true;
             this.btnIptalEt.Click += new System.EventHandler(this.btnIptalEt_Click);
+            // 
+            // buttonGeriDon
+            // 
+            this.buttonGeriDon.Location = new System.Drawing.Point(12, 551);
+            this.buttonGeriDon.Name = "buttonGeriDon";
+            this.buttonGeriDon.Size = new System.Drawing.Size(213, 56);
+            this.buttonGeriDon.TabIndex = 2;
+            this.buttonGeriDon.Text = "Geri Dön";
+            this.buttonGeriDon.UseVisualStyleBackColor = true;
+            this.buttonGeriDon.Click += new System.EventHandler(this.buttonGeriDon_Click);
             // 
             // FrmRandevuListele
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1023, 619);
+            this.Controls.Add(this.buttonGeriDon);
             this.Controls.Add(this.btnIptalEt);
             this.Controls.Add(this.dgvRandevular);
+            this.MaximizeBox = false;
             this.Name = "FrmRandevuListele";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmRandevuListele";
             this.Load += new System.EventHandler(this.FrmRandevuListele_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRandevular)).EndInit();
@@ -75,5 +91,6 @@
 
         private System.Windows.Forms.DataGridView dgvRandevular;
         private System.Windows.Forms.Button btnIptalEt;
+        private System.Windows.Forms.Button buttonGeriDon;
     }
 }
